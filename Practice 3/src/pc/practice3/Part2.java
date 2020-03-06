0d9a854a5b4f89268458bc96843159d041dd1e0f
Francisco Javier Blazquez Martinez <fcojavibm1@gmail.com>
2020-03-06 20:48:11 +0100

    Practice 3 assignment 2 finished

---
+++ Practice 3/src/pc/practice3/Part2.java
@@ -0,0 +1,34 @@
+package pc.practice3;
+
+import pc.practice3.schema.Consumer;
+import pc.practice3.schema.OneProductWarehouse;
+import pc.practice3.schema.Producer;
+import pc.practice3.schema.Warehouse;
+
+public class Part2 {
+
+    private static final int N_PROD = 10;
+    private static final int N_CONS = 10;
+    private static Warehouse wh = new OneProductWarehouse();
+
+    public static void main(String[] args) {
+
+	// Threads creation and initialization
+	Thread[] producers = new Thread[N_PROD];
+	Thread[] consumers = new Thread[N_CONS];
+
+	for(int i = 0; i < N_PROD; i++)
+	    producers[i] = new Thread(new Producer(i, wh));
+
+	for(int i = 0; i < N_CONS; i++)
+	    consumers[i] = new Thread(new Consumer(i, wh));
+
+	// Threads execution start
+	for(int i = 0; i < N_PROD; i++)
+	    producers[i].start();
+
+	for(int i = 0; i < N_CONS; i++)
+	    consumers[i].start();
+
+    }
+}
---
+++ Practice 3/src/pc/practice3/schema/OneProductWarehouse.java
@@ -0,0 +1,42 @@
+package pc.practice3.schema;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * This class implements an common interface for the communication of producers
+ * and consumers in the Producers-Consumers problem with only a space available.
+ * 
+ * @author Francisco Javier Blázquez Martínez
+ */
+public class OneProductWarehouse implements Warehouse {
+
+    private Product product;
+    private Semaphore toProduct = new Semaphore(1);
+    private Semaphore toConsume = new Semaphore(0);
+
+    @Override
+    public void store(Product product) {
+	try {
+	    toProduct.acquire();
+	} catch (InterruptedException e) { // Interruptions are not considered
+	    e.printStackTrace();
+	}
+	this.product = product;
+	toConsume.release();
+    }
+
+    @Override
+    public Product extract() {
+	try {
+	    toConsume.acquire();
+	} catch (InterruptedException e) { // Interruptions are not considered
+	    e.printStackTrace();
+	}
+	Product paux = product;
+	product = null;
+	toProduct.release();
+
+	return paux;
+    }
+
+}
