923f5a2d2c2755db5d94c0f00bbb825af13d8af6
Francisco Javier Blazquez Martinez <fcojavibm1@gmail.com>
2020-06-25 11:43:35 +0200

    Exam exercise 3 finished

--- Practice 4/src/main/java/pc/practice4/part1/schema/SeveralProductsWarehouse.java
+++ Practice 4/src/main/java/pc/practice4/part1/schema/SeveralProductsWarehouse.java
@@ -1,5 +1,8 @@
 package pc.practice4.part1.schema;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This class implements an common interface for both producers and consumers in
  * Producer-Consumers problem with a bus for several products.
@@ -8,42 +11,60 @@ package pc.practice4.part1.schema;
  */
 public class SeveralProductsWarehouse implements Warehouse {
 
-    private static final int BUS_SIZE = 10;
+    // El bus ahora es infinito
+    private List<Product> products = new ArrayList<>();
 
-    private int produceIdx = 0;
-    private int consumeIdx = 0;
-    private int nInBus = 0;
-    private volatile Product[] products = new Product[BUS_SIZE];
+    // Variables de control de estado
+    boolean produciendo;
+    int nConsumidoresConsumiendo = 0;
 
 
     @Override
-    public synchronized void store(Product product) {
-	while(nInBus == BUS_SIZE)
-	    try { wait(); } 
-	    catch (InterruptedException e) { e.printStackTrace(); }
-	
-	products[produceIdx] = product;
-	produceIdx = (produceIdx + 1) % BUS_SIZE;
-	nInBus++;
-
-	notifyAll();
+    public void store(Product product) {
+	request_produce();
+	products.add(product);
+	release_produce();
     }
 
     @Override
-    public synchronized Product extract() {
-	while(nInBus == 0)
-	    try { wait(); } 
-	    catch (InterruptedException e) { e.printStackTrace(); }
-
-	Product paux = null;
-	paux = products[consumeIdx];
-	products[consumeIdx] = null;
-	products = products;
-	consumeIdx = (consumeIdx + 1) % BUS_SIZE;
-	nInBus--;
-
-	notifyAll();
+    public Product extract(int idx) {
+	request_consume();
+	Product paux = products.get(idx);
+	release_consume();
 	return paux;
     }
 
+    private synchronized void request_produce() {
+	// A diferencia del ejercicio anterior, donde nosotros controlábamos el mutex y
+	// hacíamos paso de testigo, aquí si que tenemos que controlar si hay algún
+	// productorproduciendo (segunda condición while)
+	while(nConsumidoresConsumiendo > 0 || produciendo)
+	    try { wait(); } 
+	    catch (InterruptedException e) { e.printStackTrace(); }
+	produciendo = true;
+    }
+
+    private synchronized void release_produce() {
+	produciendo = false;
+	
+	// Los productores notifican una vez han terminado a consumidores y productores en espera.
+	// (En el ejercicio anterior se daba preferencia a los productores en este punto)
+	notifyAll();
+    }
+
+    private synchronized void request_consume() {
+	while(produciendo)
+	    try { wait(); } 
+	    catch (InterruptedException e) { e.printStackTrace(); }
+
+	nConsumidoresConsumiendo++;
+    }
+
+    private synchronized void release_consume() {
+	nConsumidoresConsumiendo--;
+	
+	// Los consumidores no notifican mientras haya más consumidores
+	if(nConsumidoresConsumiendo == 0)
+	    notifyAll();
+    }
 }
--- Practice 4/src/main/java/pc/practice4/part1/schema/Warehouse.java
+++ Practice 4/src/main/java/pc/practice4/part1/schema/Warehouse.java
@@ -2,16 +2,8 @@ package pc.practice4.part1.schema;
 
 public interface Warehouse {
 
-    /**
-     * Stores a product in the Warehouse, if there is no space the process executing
-     * this instruction gets blocked until it's freed.
-     */
     public void store(Product product);
 
-    /**
-     * Extracts first available product. If there is no product available the
-     * process executing this instruction gets blocked until a product is added.
-     */
-    public Product extract();
+    public Product extract(int idx);
 
 }
